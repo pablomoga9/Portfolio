c98dfe342877b2608aac8088577e8397b0c64f2d
pablomoga9 <moyagarcia99@gmail.com>
2023-01-09 14:16:27 +0100

    favicon

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home/Home';
 import Skills from './Skills/Skills';
@@ -8,6 +8,10 @@ import Education from './Education/Education';
 import ParticlesBackground from "../ParticlesBackground";
 
 function Main(){
+
+  useEffect(()=>{
+    document.title = 'Pablo Moya';
+  },[])
 
     return <div className="mainContainer">
           {/* <Routes>
