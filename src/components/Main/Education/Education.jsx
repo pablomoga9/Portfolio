e9e20915d539c10d751d80602aa61a2fbe91c384
pablomoga9 <moyagarcia99@gmail.com>
2024-09-19 12:13:40 +0200

    experience update

--- src/components/Main/Education/Education.jsx
+++ src/components/Main/Education/Education.jsx
@@ -7,27 +7,56 @@ const Education = () => {
   return <><h2>Education</h2>
     <section id="education">
     <article data-aos="fade-right" data-aos-duration="1500">
+      <h3>Web Applications Development Technique</h3>
+      <p className="company">CESUR</p>
+      <p>sep. 2023 - mar. 2025</p>
+      <ul>
+        <li>
+         - HTML and CSS for structuring and styling web pages.
+        </li>
+        <li>
+         - Java and Javascript languages.
+        </li>
+        <li>
+         - Back-end development using PHP, including interaction
+        with databases and server-side form processing.
+        </li>
+        <li>
+         - SQL and relational database management with MySQL.
+        </li>
+        <li>
+         - Competency in version control using Git.
+        </li>
+        <li>
+         - NoSQL databases
+        </li>
+        <li>
+         - Knowledge of server deployment and management.
+        </li>
+      </ul>
+    </article>
+    <article data-aos="fade-right" data-aos-duration="1500">
       <h3>Fullstack Development Bootcamp</h3>
       <p className="company">The Bridge</p>
       <p>jun. 2022 - nov. 2022</p>
       <ul>
         <li>
-          Layout: HTML5, CSS3, Flexbox, SASS, Bootstrap, MUi y diseño responsive.
+          - Layout: HTML5, CSS3, Flexbox, SASS, Bootstrap, MUi y diseño responsive.
         </li>
         <li>
-          Javascript (ES6).
+          - Javascript (ES6).
         </li>
         <li>
-          Database with MongoDB, PostgreSQL, SQLite and Firebase.
+          - Database with MongoDB, PostgreSQL, SQLite and Firebase.
         </li>
         <li>
-          Develop SPA with React.
+          - Develop SPA with React.
         </li>
         <li>
-          SCRUM methodology.
+          - SCRUM methodology.
         </li>
         <li>
-          Backend Development with NodeJS and ExpressJS
+          - Backend Development with NodeJS and ExpressJS
         </li>
       </ul>
     </article>
@@ -35,6 +64,17 @@ const Education = () => {
       <h3>Videogame Development Technique</h3>
       <p className="company">Centro Universitario EUSA</p>
       <p>sept. 2020 - may. 2022</p>
+      <ul>
+        <li>
+          - C# Programming and use of Unity Engine.
+        </li>
+        <li>
+          - C++ Programming and use of Unreal Engine.
+        </li>
+        <li>
+          - 3D Modeling and VFX.
+        </li>
+      </ul>
     </article>
     </section>
    
--- src/components/Main/Experience/Experience.jsx
+++ src/components/Main/Experience/Experience.jsx
@@ -5,35 +5,75 @@ const Experience = () => {
     <h2 className="expTitle">Experience</h2>
     <section id="experience">
     <article data-aos="fade-right" data-aos-duration="1500">
+      <h3>.NET Developer</h3>
+      <div className="technologies expTechno">
+        <img src="https://robertonovelo.com/rnawp/wp-content/uploads/2016/02/asp_net.png" alt="" title="ASP.NET Core" />
+        <img src="https://cdn-icons-png.flaticon.com/512/6132/6132221.png" alt="" title="C#" />
+        <img src="https://static-00.iconduck.com/assets.00/javascript-js-icon-2048x2048-nyxvtvk0.png" alt="" title="Javascript"/>
+        <img src="https://static-00.iconduck.com/assets.00/flutter-icon-2048x2048-ufx4idi8.png" alt="" title="Flutter"/>
+        <img src="https://pcr.cloud-mercato.com/static/img/logo/google.png" alt="" title="Google Cloud Platform"/>
+        <img src="https://img.icons8.com/?size=512&id=62452&format=png" alt="" title="Firebase"/>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png" title="PostgreSQL" alt="" />
+        <img src="https://static-00.iconduck.com/assets.00/linux-icon-2048x2048-sy06t4un.png" alt="" title="Linux"/>
+      </div>
+      <p className="company">LDA AudioTech</p>
+      <p>jan. 2023 - Now</p>
+      <ul>
+        <li>
+        - Development of web and desktop applications using
+        ASP.NET Core and .NET Framework 4.0 & 4.8.
+        </li>
+        <li>
+          - C# & Visual Basic .NET languages.
+        </li>
+        <li>
+        - Utilization of JavaScript (jQuery), HTML, and CSS for frontend
+        development.
+        </li>
+        <li>
+        - Proficiency in Dart and Flutter for cross-platform mobile
+        application development.
+        </li>
+        <li>
+          - Experience with Google Cloud Platform and Firebase for
+          cloud services and backend development.
+        </li>
+        <li>
+          - SQL, LINQ and PostgreSQL for database management.
+        </li>
+        <li>
+          - Linux operating system
+        </li>
+        <li>
+          - SCRUM Methodology
+        </li>
+      </ul>
+    </article>
+    <article data-aos="fade-right" data-aos-duration="1500">
       <h3>Unity & Backend Developer</h3>
       <div className="technologies expTechno">
-        <img src="https://preview.redd.it/tu3gt6ysfxq71.png?auto=webp&s=10ab55d9dc09e7ed6ea59bd5916800a5272d5969" alt="" />
-        <img src="https://cdn-icons-png.flaticon.com/512/6132/6132221.png" alt="" />
-        <img src="https://cdn-icons-png.flaticon.com/512/919/919830.png" alt="" />
-        <img src="https://friconix.com/jpg/fi-cwluxx-symfony.jpg" alt="" />
+        <img src="https://preview.redd.it/tu3gt6ysfxq71.png?auto=webp&s=10ab55d9dc09e7ed6ea59bd5916800a5272d5969" alt="" title="Unity"/>
+        <img src="https://cdn-icons-png.flaticon.com/512/6132/6132221.png" alt="" title="C#"/>
+        <img src="https://cdn-icons-png.flaticon.com/512/919/919830.png" alt="" title="PHP"/>
+        <img src="https://friconix.com/jpg/fi-cwluxx-symfony.jpg" alt="" title="Symfony"/>
       </div>
       <p className="company">Multiplicalia</p>
       <p>jan. 2022 - jul. 2022</p>
       <ul>
         <li>
-          Environment development with Unity engine and C# language.
+          - Environment development with Unity engine and C# language.
         </li>
         <li>
-          Version control with Collaborate and later with Plastic
+          - Version control with Collaborate and later with Plastic
         </li>
         <li>
-          API development with PHP framework, Symfony
+          - API development with PHP framework, Symfony
         </li>
         <li>
-          SCRUM Methodology
+          - SCRUM Methodology
         </li>
       </ul>
     </article>
-    {/* <article>
-      <h3>Catering</h3>
-      <p className="company">Icecobar</p>
-      <p>jul. 2019 - nov. 2020</p>
-    </article> */}
   </section>
   </>
 };
